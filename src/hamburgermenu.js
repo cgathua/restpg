c848bdaf5000ece70627637c77259f0a50031329
cgathua <114153640+cgathua@users.noreply.github.com>
2024-05-29 08:28:08 -0400

    finished: project

---
+++ src/hamburgermenu.js
@@ -0,0 +1,19 @@
+function hamburgerMenu() {
+    const menuBtn = document.querySelector('#menu-btn');
+    const navMenu = document.querySelector('#navbar');
+
+    menuBtn.addEventListener('click', () => {
+        menuBtn.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    });
+
+    document.querySelectorAll('.nav-link').forEach(li => {
+        li.addEventListener('click', () => {
+            menuBtn.classList.remove('active');
+            navMenu.classList.remove('active');
+        })
+    })
+
+}
+
+export default hamburgerMenu;
---
+++ src/lastsection.js
@@ -0,0 +1,23 @@
+
+function lastSection(){
+    const content = document.getElementById('content');// why choose us section
+    const lastSection =
+        `
+    <section id="why_choose_us" class="trust" aria-label="why choose us">
+    <figure class="container" aria-label="why choose us content">
+    
+        <article class="text">
+            <h3>Why choose us?</h3>
+            <p>All of our beef & chicken comes from animals, that have not been administered hormones or
+                antibiotics.
+            </p>
+            <p>Sign up below, to receive coupons.</p>
+        </article>
+    
+    </figure>
+    </section>
+    `
+    content.insertAdjacentHTML('beforeend', lastSection);
+}
+
+export default lastSection;
